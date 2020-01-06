120b3669926f9134beb7af57afd1100d16f6e907
Dennis Ouyang <den2net@yahoo.com>
2020-01-06 00:15:23 +0000

    saving all changes up to 01-05-20 to master

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,24 @@
 require "projectSorter.rb"
-# before_action :authenticate_user!
 
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :get, :update]
+
   def index
     @projects = ProjectSorter.sort(params)
   end
 
   def new
-    
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :team_members, :description)
   end
 end
