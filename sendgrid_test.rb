f6866386efd9a6fbc46e1ef2c24a7a80a6e0be86
Dennis Ouyang <den2net@yahoo.com>
2020-02-06 01:34:05 +0000

    sendgrid test

--- sendgrid_test.rb
+++ sendgrid_test.rb
@@ -1,8 +1,8 @@
+require 'sendgrid-ruby'
+include SendGrid
 
 # using SendGrid's Ruby Library
     # https://github.com/sendgrid/sendgrid-ruby
-    require 'sendgrid-ruby'
-    include SendGrid
 
     from = Email.new(email: 'test@example.com')
     to = Email.new(email: 'test@example.com')
@@ -10,7 +10,7 @@
     content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
     mail = Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
     puts response.body
