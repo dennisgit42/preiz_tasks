4457ac89096712be48146681bac5fa11a67cf5be
Dennis Ouyang <den2net@yahoo.com>
2020-02-06 10:12:30 +0000

    deleted sendgrid_test.rb, problem is yahoo blocking emails

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,4 @@
 class Project < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3, maximum: 30}
     validates :description, presence: true, length: {maximum: 500}
-
-    require 'sendgrid-ruby'
-    include SendGrid
-
-# using SendGrid's Ruby Library
-    # https://github.com/sendgrid/sendgrid-ruby
-
-    from = Email.new(email: 'test@example.com')
-    to = Email.new(email: 'den2net@yahoo.com')
-    subject = 'Sending with SendGrid is Fun'
-    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
 end
